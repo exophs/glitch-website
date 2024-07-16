33ad8e6dfae4972e5f9f50eadf9382248d81f564
Glitch (glitch-hello-website) <none>
2024-07-16 00:17:35 +0000

    🙋🚴 Checkpoint ./README.md:69146084/5854 ./index.html:69146084/86186 ./script.js:69146084/40

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 
 // Print a message in the browser's dev tools console each time the page loads
 // Use your menus or right-click / control-click and choose "Inspect" > "Console"
-console.log("Hello 🌎");
+console.log("you're gay for looking at this");
 
 /* 
 Make the "Click me!" button move when the visitor clicks it:
